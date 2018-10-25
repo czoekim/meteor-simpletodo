e140d025b510495f12c2c7e43e3701b242d4c1d9
Zoe Kim <czoekim@Zoes-MacBook-Pro-3.local>
2018-10-25 18:07:13 -0400

    Modify App.js and setup Mongo and server

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tasks } from '../api/tasks.js';
 import Task from './Task.js';
 
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
+class App extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    ReactDOM.findDOMNode(this.refs.textInput).value='';
   }
+
   renderTasks() {
-    return this.getTasks().map(task => {
-      <Task key={task._id} task={task} />
+    return this.props.tasks.map(task => {
+      return <Task key={task._id} task={task} />
     });
   }
   render() {
@@ -19,6 +27,13 @@ export default class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
@@ -28,3 +43,9 @@ export default class App extends Component {
     );
   }
 }
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}, { sort: { createdAt: -1} }).fetch(),
+  };
+})(App);
