55b7e08360b5e289e15f77cb5f953f0b5fb70129
Zoe Kim <czoekim@Zoes-MacBook-Pro-3.local>
2018-10-25 22:19:34 -0400

    Add update and remove functionality to tasks

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 export default class Task extends Component {
+  toggleChecked() {
+    // update function takes two arguments:
+    // 1) selector that identifies a subset of the Collection
+    // 2) update parameter that specifies what should be done to the matched objects
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked },
+    });
+  }
+  deleteThisTask() {
+    Tasks.remove(this.props.task._id);
+  }
   render() {
+    const taskClassName = this.props.task.checked ? 'checked' : '';
     return (
-      <li>{this.props.task.text}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisSTask.bind(this)}>
+          &times;
+        </button>
+        <input
+          type="checkbox"
+          readOnly
+          checked={!!this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+        <span className="text">{this.props.task.text}</span>
+      </li>
     );
   }
 }
